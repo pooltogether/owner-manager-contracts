46976ceb2a556c6c6842003efd5be9c7d816b511
Pierrick Turelier <pierrick@turelier.com>
2021-09-15 21:03:16 -0500

    feat(contracts): add two steps ownership transfer

--- contracts/OwnerOrManager.sol
+++ contracts/OwnerOrManager.sol
@@ -1,46 +1,86 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity >= 0.6.0 <= 0.8.6;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Context.sol";
+pragma solidity ^0.8.0;
+
+import "./external/openzeppelin/Ownable.sol";
 
 /**
 *  @title Abstract ownable contract with additional manager role
  * @notice Contract module based on Ownable which provides a basic access control mechanism, where
- * there is an account (a draw manager for example) that can be granted exclusive access to
- * specific functions.
+ * there is an owner and a manager that can be granted exclusive access to specific functions.
+ *
+ * By default, the owner is the deployer of the contract.
+ *
+ * The owner account is set through a two steps process.
+ *      1. The current `owner` calls {transferOwnership} to set a `pendingOwner`
+ *      2. The `pendingOwner` calls {acceptOwnership} to accept the ownership transfer
  *
  * The manager account needs to be set using {setManager}.
  *
  * This module is used through inheritance. It will make available the modifier
  * `onlyManager`, which can be applied to your functions to restrict their use to
  * the manager.
+ *
+ *
  */
-abstract contract OwnerOrManager is Context, Ownable {
+abstract contract OwnerOrManager is Ownable {
 
     address private _manager;
+    address private _pendingOwner;
 
     /**
-     * @dev Emitted when _manager has been changed.
-     * @param newManager new _manager address.
+     * @dev Emitted when `_manager` has been changed.
+     * @param newManager new `_manager` address.
      */
     event ManagerTransferred(address indexed newManager);
 
     /**
-     * @notice Gets current _manager.
-     * @dev Returns current _manager address.1
-     * @return Current _manager address.
+     * @notice Gets current `_manager`.
+     * @return Current `_manager` address.
      */
     function manager() public view virtual returns (address) {
         return _manager;
     }
 
     /**
+     * @notice Gets current `_pendingOwner`.
+     * @return Current `_pendingOwner` address.
+     */
+    function pendingOwner() external view virtual returns (address) {
+        return _pendingOwner;
+    }
+
+    /**
+    * @dev Throws if called by any account other than the `pendingOwner`.
+    */
+    modifier onlyPendingOwner() {
+        require(_msgSender() == _pendingOwner, "OwnerOrManager/caller-not-pendingOwner");
+        _;
+    }
+
+    /**
      * @dev Throws if called by any account other than the manager.
      */
     modifier onlyManagerOrOwner() {
-        require(manager() == _msgSender() || owner() == _msgSender(), "Manager/caller-not-manager-or-owner");
+        require(manager() == _msgSender() || owner() == _msgSender(), "OwnerOrManager/caller-not-manager-or-owner");
         _;
+    }
+
+    /**
+    * @dev Allows current owner to set the pendingOwner address.
+    * @param _newOwner Address to transfer ownership to.
+    */
+    function transferOwnership(address _newOwner) public override onlyOwner {
+        require(_newOwner != address(0), "OwnerOrManager/pendingOwner-not-zero-address");
+        _pendingOwner = _newOwner;
+    }
+
+    /**
+    * @dev Allows the pendingOwner address to finalize the transfer.
+    */
+    function claimOwnership() external onlyPendingOwner {
+        _setOwner(_pendingOwner);
+        _pendingOwner = address(0);
     }
 
     /**
@@ -49,20 +89,20 @@ abstract contract OwnerOrManager is Context, Ownable {
      * @param _newManager New _manager address.
      * @return Boolean to indicate if the operation was successful or not.
      */
-    function setManager(address _newManager) public onlyOwner returns (bool) {
-        _setManager(_newManager);
+    function setManager(address _newManager) external onlyOwner returns (bool) {
+        return _setManager(_newManager);
     }
 
     /**
      * @notice Set or change of manager.
-     * @dev Throws if called by any account other than the owner.
      * @param _newManager New _manager address.
      * @return Boolean to indicate if the operation was successful or not.
      */
     function _setManager(address _newManager) internal returns (bool) {
         address _previousManager = _manager;
-        require(_newManager != address(0), "Manager/manager-not-zero-address");
-        require(_newManager != _previousManager, "Manager/existing-manager-address");
+
+        require(_newManager != address(0), "OwnerOrManager/manager-not-zero-address");
+        require(_newManager != _previousManager, "OwnerOrManager/existing-manager-address");
 
         _manager = _newManager;
 
--- test/OwnerOrManager.test.ts
+++ test/OwnerOrManager.test.ts
@@ -3,38 +3,125 @@ import { Contract, ContractFactory, Signer, Wallet } from 'ethers';
 import { ethers } from 'hardhat';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 
+const { constants } = ethers;
+const { AddressZero } = constants;
+
 describe('OwnerOrManager', () => {
-    let ownerOrManager: Contract
+    let ownerOrManager: Contract;
 
-    let wallet1: SignerWithAddress, wallet2: SignerWithAddress, wallet3: SignerWithAddress, wallet4: SignerWithAddress
+    let contractsOwner: SignerWithAddress,
+        wallet2: SignerWithAddress,
+        wallet3: SignerWithAddress,
+        wallet4: SignerWithAddress;
 
-    beforeEach(async () =>{
-        [wallet1, wallet2, wallet3, wallet4] = await ethers.getSigners()
+    beforeEach(async () => {
+        [contractsOwner, wallet2, wallet3, wallet4] = await ethers.getSigners();
 
-        const ownerOrManagerFactory: ContractFactory = await ethers.getContractFactory("OwnerOrManagerHarness")
-        ownerOrManager = await ownerOrManagerFactory.deploy()
-    })
-    describe("setManager()", ()=>{
-        it('Can set manager', async () => {
-            expect(await ownerOrManager.setManager(wallet2.address)).to.emit(ownerOrManager, 'ManagerTransferred')
-            expect(await ownerOrManager.manager()).to.equal(wallet2.address)
-        })
-    })
+        const ownerOrManagerFactory = await ethers.getContractFactory('OwnerOrManagerHarness');
 
-    describe("onlyManagerOrOwner()", ()=>{
-        it('non owner non manager cannot call permissioned function', async () => {
-            await expect(ownerOrManager.connect(wallet3).protectedFunction()).to.be.revertedWith("Manager/caller-not-manager-or-owner")       
-        })
+        ownerOrManager = await ownerOrManagerFactory.deploy();
+    });
 
-        it('non owner non manager cannot call permissioned function', async () => {
-            await expect(ownerOrManager.protectedFunction()).to.emit(ownerOrManager, "ReallyCoolEvent")  
-            await ownerOrManager.setManager(wallet2.address)      
-            await expect(ownerOrManager.connect(wallet2).protectedFunction()).to.emit(ownerOrManager, "ReallyCoolEvent")
-        })
+    describe('owner()', () => {
+        it('should be deployer address by default', async () => {
+            expect(await ownerOrManager.owner()).to.equal(contractsOwner.address);
+        });
+    });
 
-        it('manager cannot set new manage', async ()=> {
-            await expect(ownerOrManager.connect(wallet2).protectedFunction()).to.be.reverted
-        })
-    })
+    describe('manager()', () => {
+        it('should be address zero by default', async () => {
+            expect(await ownerOrManager.manager()).to.equal(AddressZero);
+        });
+    });
 
-})
+    describe('pendingOwner()', () => {
+        it('should be address zero by default', async () => {
+            expect(await ownerOrManager.pendingOwner()).to.equal(AddressZero);
+        });
+    });
+
+    describe('transferOwnership()', () => {
+        it('should transfer ownership to wallet2', async () => {
+            expect(await ownerOrManager.transferOwnership(wallet2.address));
+            expect(await ownerOrManager.pendingOwner()).to.equal(wallet2.address);
+        });
+
+        it('should fail to transfer ownership to address zero', async () => {
+            await expect(ownerOrManager.transferOwnership(AddressZero)).to.be.revertedWith(
+                'OwnerOrManager/pendingOwner-not-zero-address',
+            );
+        });
+
+        it('should fail to transfer ownership if not currently owner', async () => {
+            await expect(
+                ownerOrManager.connect(wallet2).transferOwnership(wallet3.address),
+            ).to.be.revertedWith('Ownable: caller is not the owner');
+        });
+    });
+
+    describe('claimOwnership()', () => {
+        beforeEach(async () => {
+            await ownerOrManager.transferOwnership(wallet2.address);
+        });
+
+        it('should be claimed by pending owner', async () => {
+            expect(await ownerOrManager.connect(wallet2).claimOwnership())
+                .to.emit(ownerOrManager, 'OwnershipTransferred')
+                .withArgs(contractsOwner.address, wallet2.address);
+
+            expect(await ownerOrManager.owner()).to.equal(wallet2.address);
+            expect(await ownerOrManager.pendingOwner()).to.equal(AddressZero);
+        });
+
+        it('should fail to claim ownership if not pending owner', async () => {
+            await expect(ownerOrManager.connect(wallet3).claimOwnership()).to.be.revertedWith(
+                'OwnerOrManager/caller-not-pendingOwner',
+            );
+        });
+    });
+
+    describe('setManager()', () => {
+        it('should set manager', async () => {
+            expect(await ownerOrManager.setManager(wallet2.address))
+                .to.emit(ownerOrManager, 'ManagerTransferred')
+                .withArgs(wallet2.address);
+
+            expect(await ownerOrManager.manager()).to.equal(wallet2.address);
+        });
+
+        it('should fail to set manager to address zero', async () => {
+            await expect(ownerOrManager.setManager(AddressZero)).to.be.revertedWith(
+                'OwnerOrManager/manager-not-zero-address',
+            );
+        });
+
+        it('should fail to set manager if already manager', async () => {
+            await ownerOrManager.setManager(wallet2.address);
+
+            await expect(ownerOrManager.setManager(wallet2.address)).to.be.revertedWith(
+                'OwnerOrManager/existing-manager-address',
+            );
+        });
+    });
+
+    describe('onlyManagerOrOwner()', () => {
+        it('should fail to call permissioned function if not owner or manager', async () => {
+            await expect(ownerOrManager.connect(wallet3).protectedFunction()).to.be.revertedWith(
+                'Manager/caller-not-manager-or-owner',
+            );
+        });
+
+        it('should call permissioned function if owner or manager', async () => {
+            await expect(ownerOrManager.protectedFunction()).to.emit(
+                ownerOrManager,
+                'ReallyCoolEvent',
+            );
+
+            await ownerOrManager.setManager(wallet2.address);
+            await expect(ownerOrManager.connect(wallet2).protectedFunction()).to.emit(
+                ownerOrManager,
+                'ReallyCoolEvent',
+            );
+        });
+    });
+});
